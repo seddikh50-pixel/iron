e870d054c56fac2ce7d59fa56e35ee9b336b76a8
seddikh50-pixel <seddikh50@gmail.com>
2026-06-14 10:52:39 +0100

    siraj

--- app/page.jsx
+++ app/page.jsx
@@ -44,7 +44,7 @@ export default function Home() {
         end: "bottom top",
         scrub: 1,
         pin: true,
-        markers: true,
+        // markers: true,
       },
     });
 
@@ -70,12 +70,12 @@ export default function Home() {
     <div>
       {/* <div className="w-full h-96"></div> */}
       <div ref={videoWrapper1} className=" h-screen w-full  ">
-        <video ref={videoRef1} src="output2.mp4" className="h-full w-full object-cover"></video>
+        <video ref={videoRef1} muted playsInline preload="auto" src="output2.mp4" className="h-full w-full object-cover"></video>
 
       </div>
 
       <div ref={videoWrapper2} className=" h-screen w-full  ">
-        <video ref={videoRef2} src="output.mp4" className="h-full w-full object-cover"></video>
+        <video ref={videoRef2} muted playsInline preload="auto" src="output.mp4" className="h-full w-full object-cover"></video>
 
       </div>
     </div>
