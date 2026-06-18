4f1ef9b98b8bb92cac001bee225923aee3ae82c4
seddik <seddikh50@gmail.com>
2026-06-18 11:38:57 +0100

    testing counter

--- app/page.jsx
+++ app/page.jsx
@@ -4,12 +4,13 @@ import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/src/SplitText";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 gsap.registerPlugin(ScrollTrigger, useGSAP, SplitText);
 
 
 export default function Home() {
+  const  [counter, setCounter ] = useState(0)
   const videoRef1 = useRef()
   const videoWrapper1 = useRef()
   const videoRef2 = useRef()
@@ -51,6 +52,10 @@ export default function Home() {
           scrub: 1,
           pin: true,
           markers: true,
+          onUpdate : (self) => {
+            setCounter(self.progress * 100) 
+            console.log(counter)
+          }
         },
       });
 
@@ -96,6 +101,7 @@ export default function Home() {
     <div>
 
       <div ref={videoWrapper1} className=" h-screen bg-amber-400 w-full flex justify-start items-center  ">
+        <div className="fixed text-amber-300 right-3 top-3 z-30">{counter} %</div>
        <div className="absolute top-20 right-5 bottom-10 left-5">
           <div className=" absolute w-7 h-7 border-t-1 border-l-1  border-amber-300"></div>
           <div className=" absolute w-7 h-7 border-r-1 border-t-1  right-0 border-amber-300"></div>
@@ -103,8 +109,8 @@ export default function Home() {
           <div className=" absolute w-7 h-7 border-l-1 border-b-1 bottom-0 border-amber-300"></div>
         </div>
         <div className="seddik absolute text-white text-6xl z-10 pl-10 ">
-          <h1 className="mb-5">Hamaidi </h1>
-          <h1 className="text font-bold text-8xl">Abou <span className="text-[#d4af37]">Seddik</span></h1>
+          <h1 className="mb-5 text-8xl">I am </h1>
+          <h1 className="text font-bold text-8xl">Ham Abou <span className="text-[#d4af37]">Seddik</span></h1>
 
         </div>
         <video ref={videoRef1} muted playsInline preload="auto" src="output2.mp4" className="h-full w-full object-cover"></video>
