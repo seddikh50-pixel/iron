8a2d9601ffe3716baeac424ebd2fa1cea7970c30
seddik <seddikh50@gmail.com>
2026-06-18 11:31:03 +0100

    add boxes

---
+++ app/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Headers = () => {
+  return (
+    <div className='h-20 fixed top-0 z-10 bg-black/95 border-b-[0.5px] border-amber-50/5  w-full'>
+      
+    </div>
+  )
+}
+
+export default Headers
--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,11 @@
 import Image from "next/image";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
-
+import { SplitText } from "gsap/src/SplitText";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useRef } from "react";
 
-gsap.registerPlugin(ScrollTrigger, useGSAP);
+gsap.registerPlugin(ScrollTrigger, useGSAP, SplitText);
 
 
 export default function Home() {
@@ -18,10 +18,17 @@ export default function Home() {
     const video1 = videoRef1.current;
     const video2 = videoRef2.current;
 
-    const videos = [video1, video2]
+    const split = SplitText.create(".text", {
+      type: "chars"
+    })
 
-    const duration = video1.duration
-    console.log(duration);
+    gsap.from(split.chars, {
+      opacity: 0,
+      stagger: 0.1,
+
+    })
+
+
 
 
 
@@ -53,14 +60,14 @@ export default function Home() {
         ease: "none",
       });
 
-        tl1.to(".seddik", {
+      tl1.to(".seddik", {
         x: 100,
-        duartion: 2 ,
+        duartion: 2,
         ease: "none",
       });
     };
 
-    
+
 
 
     video2.onloadedmetadata = () => {
@@ -87,20 +94,34 @@ export default function Home() {
   }, []);
   return (
     <div>
-      {/* <div className="w-full h-96"></div> */}
-      <div ref={videoWrapper1} className=" h-screen w-full  ">
-          <div className="seddik absolute text-white text-6xl z-10">
-         hamaidi seddik 
-       </div>
+
+      <div ref={videoWrapper1} className=" h-screen bg-amber-400 w-full flex justify-start items-center  ">
+       <div className="absolute top-20 right-5 bottom-10 left-5">
+          <div className=" absolute w-7 h-7 border-t-1 border-l-1  border-amber-300"></div>
+          <div className=" absolute w-7 h-7 border-r-1 border-t-1  right-0 border-amber-300"></div>
+          <div className=" absolute w-7 h-7  border-r-1 border-b-1  right-0 bottom-0 border-amber-300"></div>
+          <div className=" absolute w-7 h-7 border-l-1 border-b-1 bottom-0 border-amber-300"></div>
+        </div>
+        <div className="seddik absolute text-white text-6xl z-10 pl-10 ">
+          <h1 className="mb-5">Hamaidi </h1>
+          <h1 className="text font-bold text-8xl">Abou <span className="text-[#d4af37]">Seddik</span></h1>
+
+        </div>
         <video ref={videoRef1} muted playsInline preload="auto" src="output2.mp4" className="h-full w-full object-cover"></video>
-     
+
       </div>
 
-      <div ref={videoWrapper2} className=" h-screen w-full  ">
+      <div ref={videoWrapper2} className=" relative h-screen w-full  ">
+          <div className="absolute top-20 right-5 bottom-10 left-5">
+          <div className=" absolute w-7 h-7 border-t-1 border-l-1  border-amber-300"></div>
+          <div className=" absolute w-7 h-7 border-r-1 border-t-1  right-0 border-amber-300"></div>
+          <div className=" absolute w-7 h-7  border-r-1 border-b-1  right-0 bottom-0 border-amber-300"></div>
+          <div className=" absolute w-7 h-7 border-l-1 border-b-1 bottom-0 border-amber-300"></div>
+        </div>
         <video ref={videoRef2} muted playsInline preload="auto" src="output.mp4" className="h-full w-full object-cover"></video>
 
       </div>
-      <div className="w-full h-96 bg-blue-600"></div>
+      {/* <div className="w-full h-96 "></div> */}
     </div>
   );
 }
