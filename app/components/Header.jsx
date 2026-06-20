994a177ebc90645596ce6987481cfea5c0499e6d
seddik <seddikh50@gmail.com>
2026-06-20 14:45:46 +0100

    working on skills

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,11 +1,20 @@
 import { useGSAP } from '@gsap/react'
+import { MoveUpRight } from 'lucide-react';
 import React, { useEffect } from 'react'
+import { Circle } from 'lucide-react';
+import Link from 'next/link';
+
 
 const Headers = ({tl}) => {
 
 useEffect(() => {
   if (!tl) return;
 
+
+  // tl.from(".header", {
+  //   y: 100,
+  //   ease: "none",
+  // }, 0);
   tl.to(".header", {
     backgroundColor: "black",
     ease: "none",
@@ -13,9 +22,17 @@ useEffect(() => {
 
 }, [tl]);
   return (
-    <div className='header h-20 fixed top-0 z-10 bg-black/5 border-b-[0.5px] border-amber-50/5  w-full'>
-      
-    </div>
+    <header className='header h-20 fixed top-0 z-10 bg-black/5 border-b-[0.5px] border-amber-50/5 flex justify-between items-center px-20 text-gray-300  w-full'>
+        <div className='flex items-center gap-2'><Circle fill="yellow" stroke='yellow' size={15}/> Hamaidi &ensp;  / &ensp; Abou Seddik</div>
+        <div className='flex gap-10'>
+          <h1>SYSTEMS</h1>
+          <h1>ARCHIVE</h1>
+        </div>
+        <div>
+        
+          <Link className='border  px-4 py-1 border-gray-700 rounded-4xl flex gap-1 text-sm items-center' href={"https://www.linkedin.com/in/hamaidi-seddik-28ab4634a/"} target="_blank"><span>CONTACT</span> <MoveUpRight size={20} /></Link>
+        </div>
+    </header>
   )
 }
 
--- app/page.jsx
+++ app/page.jsx
@@ -4,19 +4,39 @@ import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/src/SplitText";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Header from "./components/Header";
+import { Circle } from 'lucide-react';
+import { FaReact } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { FaJsSquare } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa6";
+import { SiNextdotjs } from "react-icons/si";
+import { FaNode } from "react-icons/fa";
+import { SiGsap } from "react-icons/si";
+
+
 
 gsap.registerPlugin(ScrollTrigger, useGSAP);
 
 
 export default function Home() {
   const [tl, setTl] = useState(null)
-    const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0)
   const videoRef1 = useRef()
   const videoWrapper1 = useRef()
   const videoRef2 = useRef()
   const videoWrapper2 = useRef()
+  const iconRef = useRef()
+  const [children, setChildren] = useState([]);
+
+  useEffect(() => {
+    if (!iconRef) return null
+    const children = Array.from(iconRef.current.children)
+
+
+
+  }, []);
   useGSAP(() => {
     const video1 = videoRef1.current;
     const video2 = videoRef2.current;
@@ -39,6 +59,7 @@ export default function Home() {
     if (!video2) return;
 
 
+    const duration = video1.duration
 
 
 
@@ -50,10 +71,9 @@ export default function Home() {
       scrollTrigger: {
         trigger: videoWrapper1.current,
         start: "top top",
-        end: "bottom top",
+        end: "+=1000",
         scrub: 1,
         pin: true,
-        markers: true,
 
         onUpdate: (self) => {
           setCounter(self.progress * 100); // من 0 إلى 1
@@ -61,18 +81,52 @@ export default function Home() {
       }
     });
 
-
     tl1.to(video1, {
-      currentTime: 10,
+      currentTime: duration,
       ease: "none",
     });
 
-    tl1.to(".seddik", {
-      x: 100,
+    tl1.fromTo(".message",
+      {
+        y: 40,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        stagger: 1,
+        ease: "none",
+      },
+      "<" // يبدأ مع الفيديو
+    );
+
+    tl1.to(".message", {
+      y: -40,
+      opacity: 0,
+      duration: 1,
+      stagger: 1,
+      ease: "none",
+    });
+
+    tl1.to(".text1", {
+      y: -100,
+      opacity: 0,
       duration: 2,
       ease: "none",
     }, "<");
-    // };
+    tl1.from(".text2", {
+      y: 100,
+      opacity: 0,
+      duration: 2,
+      ease: "none",
+    });
+
+
+
+
+
+
 
     setTl(tl1)
 
@@ -87,14 +141,22 @@ export default function Home() {
         end: "bottom top",
         scrub: 1,
         pin: true,
-        // markers: true,
       },
     });
 
     tl2.to(video2, {
       currentTime: 10,
 
-    });
+    })
+
+    // console.log(iconRef.current.children)
+    // console.log(gsap.utils.toArray(iconRef.current.children))
+    // gsap.utils.toArray(iconRef.current.children).forEach((ic, i) => {
+    //   tl2.from(ic, {
+    //     y: 200 * i
+    //   })
+    // })
+
 
 
 
@@ -102,38 +164,75 @@ export default function Home() {
 
   }, []);
   return (
-    <div>
+    <div className="overflow-x-hidden ">
 
       <div ref={videoWrapper1} className=" h-screen bg-amber-400 w-full flex justify-start items-center  ">
         <Header tl={tl} />
 
-        <div className="fixed text-amber-300 right-9 top-24 z-50 trns duration-200">{Math.floor(counter)}%</div>
+        <div className="fixed text-amber-300 right-9 top-24 z-50 transition-all duration-200 flex justify-center items-center gap-2"><h1>{Math.floor(counter)}%</h1>       <Circle fill="yellow" size={15} />
+        </div>
         <div className="absolute top-23 right-5 bottom-10 left-5">
           <div className=" absolute w-7 h-7 border-t-1 border-l-1  border-amber-300"></div>
           <div className=" absolute w-7 h-7 border-r-1 border-t-1  right-0 border-amber-300"></div>
           <div className=" absolute w-7 h-7  border-r-1 border-b-1  right-0 bottom-0 border-amber-300"></div>
           <div className=" absolute w-7 h-7 border-l-1 border-b-1 bottom-0 border-amber-300"></div>
         </div>
-        <div className="seddik absolute text-white text-6xl z-10 pl-10 ">
+        <div className="text1 absolute text-white text-6xl z-10 pl-10  overflow-hidden ">
           <h1 className="mb-5 text-8xl">I am </h1>
           <h1 className="text font-bold text-8xl">Ham Abou <span className="text-[#d4af37]">Seddik</span></h1>
+          <p className="paragraph text-2xl w-1/2 mt-10 text-gray-400">I'm a Web Developer specializing in HTML, CSS, JavaScript, React, Next.js, and Node.js. I build modern, responsive, and interactive websites and web applications, focusing on performance, clean code, and engaging user experiences.</p>
+        </div>
+        <div className="text2 absolute text-white text-6xl z-10 pl-10  overflow-hidden ">
+          <h1 className="mb-5 text-8xl">Built with </h1>
+          <h1 className="text font-bold text-8xl"> Next.js & <span className="text-[#d4af37]">GSAP</span></h1>
+          <p className="paragraph text-2xl w-1/2 mt-10 text-gray-400">Crafting interactive web experiences with modern tools. A smooth blend of performance and animation. Building fast, animated, and modern web experiences.</p>
+        </div>
+        <div className="message absolute text-white flex flex-col justify-between h-30 px-2 py-3  right-20 top-40 z-2 w-80  backdrop-blur-md  bg-[#121211]/50 rounded-lg border-[#292927] border-[0.5px] ">
+          <h1>''Building experiences, not just websites.''</h1>
+          <div className="flex justify-between px-2">
+            <h1 className=" text-white/50">Abou</h1>
+            <h1 className="text-amber-300/50 ">hamaidi seddik</h1>
+          </div>
+        </div>
+        <div className="message  absolute text-white flex flex-col justify-between h-30 px-2 py-3  right-20 top-103 z-2 w-80 backdrop-blur-md  bg-[#121211]/50 rounded-lg border-[#292927] border-[0.5px] ">
+          <h1>''Developer. Designer. Dreamer. Builder.''</h1>
+          <div className="flex justify-between px-2">
+            <h1 className=" text-white/50">Abou</h1>
+            <h1 className="text-amber-300/50 ">hamaidi seddik</h1>
+          </div>
+        </div>
 
+        <div className="message  absolute text-white flex flex-col justify-between h-30 px-2 py-3  right-20 top-165 z-2 w-80 backdrop-blur-md   bg-[#121211]/50 rounded-lg border-[#292927] border-[0.5px] ">
+          <h1>''What ends one project starts another idea.''</h1>
+          <div className="flex justify-between px-2">
+            <h1 className=" text-white/50">Abou</h1>
+            <h1 className="text-amber-300/50 ">hamaidi seddik</h1>
+          </div>
         </div>
         <video ref={videoRef1} muted playsInline preload="auto" src="output2.mp4" className="h-full w-full object-cover"></video>
 
       </div>
 
-      <div ref={videoWrapper2} className=" relative h-screen w-full  ">
-        <div className="absolute top-20 right-5 bottom-10 left-5">
+      <div ref={videoWrapper2} className=" relative h-screen w-full overflow-hidden  ">
+
+
+        <div className="absolute top-10 right-5 bottom-10 left-5">
           <div className=" absolute w-7 h-7 border-t-1 border-l-1  border-amber-300"></div>
           <div className=" absolute w-7 h-7 border-r-1 border-t-1  right-0 border-amber-300"></div>
           <div className=" absolute w-7 h-7  border-r-1 border-b-1  right-0 bottom-0 border-amber-300"></div>
           <div className=" absolute w-7 h-7 border-l-1 border-b-1 bottom-0 border-amber-300"></div>
         </div>
         <video ref={videoRef2} muted playsInline preload="auto" src="output.mp4" className="h-full w-full object-cover"></video>
-
+        <div ref={iconRef} className="icons absolute z-100 inset-5 text-white flex justify-center items-center gap-5">
+          <FaReact size={200} />
+          <FaHtml5 size={200} />
+          <FaJsSquare size={200} />
+          <FaCss3Alt size={200} />
+          <FaNode size={200} />
+          <SiGsap size={200} />
+          <SiNextdotjs size={200} />
+        </div>
       </div>
-      {/* <div className="w-full h-96 "></div> */}
     </div>
   );
 }
